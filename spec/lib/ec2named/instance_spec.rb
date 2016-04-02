37c76eafa41a2116788348c02017bb43e91f1f72
Alan Norton <me@alannorton.com>
2016-04-02 16:42:21 -0400

    make fixture data more generic

--- spec/lib/ec2named/instance_spec.rb
+++ spec/lib/ec2named/instance_spec.rb
@@ -6,12 +6,27 @@ describe Ec2named::Instance do
   let(:raw_instance) { YAML.load(File.read("spec/fixtures/i-0a1b2c3d.yaml")).first }
   subject { Ec2named::Instance.new(raw_instance) }
 
+  it "#name" do
+    expect(subject.send(:name)).to eq "staging.sucksrocks"
+  end
+
+  it "#status" do
+    expect(subject.send(:status)).to eq nil
+  end
+
+  it "#tags" do
+    expect(subject.send(:tags)).to match_array([
+      ["Name", "staging.sucksrocks"],
+      ["app", "sucksrocks"],
+      ["env", "staging"]
+    ])
+  end
+
   it "#common_tags" do
     expect(subject.send(:common_tags)).to match_array([
-      ["Name", "stage.pipeline.sucksrocks"],
+      ["Name", "staging.sucksrocks"],
       ["app", "sucksrocks"],
-      ["env", "stage"],
-      ["status", "in-use"]
+      ["env", "staging"]
     ])
   end
 end
